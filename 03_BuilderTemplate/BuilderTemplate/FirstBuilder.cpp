570a1da312df5645c23882392b08a36db30a2462
Андрій Лецкалюк <letskalyuk@rivne.itstep.org>
2019-08-22 19:40:13 +0300

    BuilderTemplatePattern

---
+++ 03_BuilderTemplate/BuilderTemplate/FirstBuilder.cpp
@@ -0,0 +1,191 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+//патерн білдер - призначений для об"єкту що складається з менших частин
+
+class Notebook
+{
+private:
+	string name;
+	map<string, string> ourmap;
+public:
+	Notebook(string n = "Notebook") : name(n)
+	{
+	}
+	string getPart(const string& key)
+	{
+		return ourmap[key];
+	}
+	void setPart(string key, string value)
+	{
+		this->ourmap[key] = value;
+	}
+	void Show()
+	{
+		for (auto el : ourmap)
+		{
+			cout << el.first << " " << el.second << endl;
+		}
+	}
+	bool CheckPart(string key)
+	{
+		return	ourmap.find(key) != ourmap.end() ? true : false;
+	}
+};
+
+class NoteBookBuilder
+{
+protected:
+	Notebook* device;
+public:
+	virtual void setMemory() = 0;
+	virtual void setHDD() = 0;
+	virtual void setMatrix() = 0;
+	virtual void setCPU() = 0;
+	Notebook* getDevice()
+	{
+		return this->device;
+	}
+};
+
+class GameNoteBook : public NoteBookBuilder
+{
+
+public:
+	GameNoteBook()
+	{
+		device = new Notebook("GameNoteBook");
+	}
+	void setMemory()
+	{
+		device->setPart("memory", "16Gb");
+	}
+	void setHDD()
+	{
+		device->setPart("HDD", "1028Gb");
+	}
+	void setMatrix()
+	{
+		device->setPart("Matrix", "HP");
+	}
+	void setCPU()
+	{
+		device->setPart("CPU", "Intel");
+	}
+};
+
+class HomeNoteBook : public NoteBookBuilder
+{
+public:
+	HomeNoteBook()
+	{
+		device = new Notebook("HomeNoteBook");
+	}
+	void setMemory()
+	{
+		device->setPart("memory", "8Gb");
+	}
+	void setHDD()
+	{
+		device->setPart("HDD", "256Gb");
+	}
+	void setMatrix()
+	{
+		device->setPart("Matrix", "Lenovo");
+	}
+	void setCPU()
+	{
+		device->setPart("CPU", "AMD");
+	}
+};
+
+class CustomNoteBook : public NoteBookBuilder
+{
+public:
+	CustomNoteBook()
+	{
+		device = new Notebook("CustumerNoteBook");
+	}
+	void setMemory()
+	{
+		device->setPart("memory", "2Gb");
+	}
+	void setHDD()
+	{
+		device->setPart("HDD", "64Gb");
+	}
+	void setMatrix()
+	{
+		device->setPart("Matrix", "HP");
+	}
+	void setCPU()
+	{
+		device->setPart("CPU", "Celeron");
+	}
+	void SetYourVariant()
+	{
+		string temp;
+		cout << "EnterCPU\n";
+		cin >> temp;
+		device->setPart("CPU", temp);
+		//temp = nullptr;
+
+		cout << "EnterHDD\n";
+		cin >> temp;
+		device->setPart("HDD", temp);
+		//temp = nullptr;
+		
+		cout << "EnterMatrix\n";
+		cin >> temp;
+		device->setPart("Matrix", temp);
+		//temp = nullptr;
+
+		cout << "EnterMemory\n";
+		cin >> temp;
+		device->setPart("Memory", temp);
+		//temp = nullptr;
+	}
+	void Show()
+	{
+		device->Show();
+	}
+};
+
+class NoteBookShop
+{
+public:
+	void CreateNoteBook(NoteBookBuilder * b)
+	{
+		
+		b->setCPU();
+		
+		b->setHDD();
+		
+		b->setMatrix();
+		
+		b->setMemory();
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	NoteBookShop* shop = new NoteBookShop();
+
+	NoteBookBuilder* builder = new GameNoteBook();
+
+	shop->CreateNoteBook(builder);
+	builder->getDevice()->Show();
+
+	CustomNoteBook* custom = new CustomNoteBook();
+	custom->SetYourVariant();
+	custom->Show();
+
+
+	system("pause");
+	return 0;
+}
